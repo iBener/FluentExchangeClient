353a9251b878405b02726f2ac7863830c5fbb180
İbrahim Bener <ibrahimbener@gmail.com>
2022-08-28 15:54:14 +0300

    0.1.5-preview Ticker weights

--- FluentExchangeClient/Exchange/Binance/Requests/BinanceFuturesRequestTicker.cs
+++ FluentExchangeClient/Exchange/Binance/Requests/BinanceFuturesRequestTicker.cs
@@ -9,9 +9,14 @@ namespace FluentExchangeClient.Exchange.Binance.Requests;
 
 class BinanceFuturesRequestTicker : BinanceBaseFuturesRequest
 {
+    private readonly string? _symbol;
+
     public BinanceFuturesRequestTicker(string? symbol = null, string? quoteSymbol = null) : base(new { symbol = symbol + quoteSymbol }, null)
     {
         Method = HttpMethod.Get;
         RequestUri = new Uri(BaseAddress, "/fapi/v1/ticker/24hr" + QueryString);
+        _symbol = $"{symbol}{quoteSymbol}";
     }
+
+    public override int Weight => String.IsNullOrEmpty(_symbol) ? 40 : 1;
 }
--- FluentExchangeClient/Exchange/Binance/Requests/BinanceRequestTicker.cs
+++ FluentExchangeClient/Exchange/Binance/Requests/BinanceRequestTicker.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace FluentExchangeClient.Exchange.Binance.Requests;
 
 class BinanceRequestTicker : BinanceBaseRequest
 {
+    private readonly string? _symbol;
+    private readonly List<string>? _symbols;
+
     public BinanceRequestTicker(string? symbol = null, string? quoteSymbol = null) : base(new { symbol = symbol + quoteSymbol }, null)
     {
         Method = HttpMethod.Get;
         RequestUri = new Uri(BaseAddress, "/api/v3/ticker/24hr" + QueryString);
+        _symbol = $"{symbol}{quoteSymbol}";
     }
+
+    public BinanceRequestTicker(List<string> symbols) : base(CreateParamsObject(symbols), null)
+    {
+        Method = HttpMethod.Get;
+        RequestUri = new Uri(BaseAddress, "/api/v3/ticker/24hr" + QueryString);
+        _symbols = symbols;
+    }
+
+    private static object? CreateParamsObject(List<string> symbols)
+    {
+        if (symbols?.Count > 0)
+        {
+            string sym = $@"[""{String.Join(@""",""", symbols)}""]";
+            return new { symbols = sym };
+        }
+        return null;
+    }
+
+    public override int Weight => 
+        !String.IsNullOrEmpty(_symbol) ? 1 :
+        _symbols?.Count switch
+        {
+            int i when i == 0 => 40,
+            int i when i > 0 && i <= 20 => 1,
+            int i when i > 20 && i <= 100 => 20,
+            _ => 40
+        };
 }
